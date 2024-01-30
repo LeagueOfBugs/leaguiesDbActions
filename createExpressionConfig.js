a798449743e31e7270d44db2f51659f240f262ea
LeagueOfBugs <andres@Andress-MBP.lan>
2024-01-30 14:46:52 -0500

    feat/ CRUD functionality for tables in DynamoDB --- alpha

---
+++ createExpressionConfig.js
@@ -0,0 +1,138 @@
+const createExpressionConfig = (tableName, model) => {
+  switch (tableName) {
+    case "matches":
+      return {
+        UpdateExpression: `
+      SET
+      awayTeam = :awayTeam,
+      #date = :date,
+      homeTeam = :homeTeam,
+      leagueId = :leagueId,
+      #location = :location,
+      #name = :name,
+      officiating = :officiating,
+      seasonId = :seasonId,
+      #time = :time
+    `,
+        ExpressionAttributeValues: {
+          ":awayTeam": model.awayTeam,
+          ":date": model.date,
+          ":homeTeam": model.homeTeam,
+          ":leagueId": model.leagueId,
+          ":location": model.location,
+          ":name": model.name,
+          ":officiating": model.officiating,
+          ":seasonId": model.seasonId,
+          ":time": model.time,
+        },
+        ExpressionAttributeNames: {
+          "#date": "date",
+          "#location": "location",
+          "#name": "name",
+          "#time": "time",
+        },
+      };
+    case "seasons":
+      return {
+        UpdateExpression: `
+      SET
+      start = :start,
+      end = :end,
+      games = :games,
+      leagueId = :leagueId,
+      #name = :name,
+      cadence = :cadence,
+      matches = :matches
+    `,
+        ExpressionAttributeValues: {
+          ":start": model.start,
+          ":end": model.end,
+          ":games": model.games,
+          ":leagueId": model.leagueId,
+          ":name": model.name,
+          ":cadence": model.cadence,
+          ":matches": model.matches,
+        },
+        ExpressionAttributeNames: {
+          "#name": "name",
+        },
+      };
+    case "players":
+      return {
+        UpdateExpression: `
+      SET
+      image = :image,
+      team = :team,
+      position = :position,
+      league = :league,
+      #name = :name,
+      agency = :agency,
+      onlineStatus = :onlineStatus,
+    `,
+        ExpressionAttributeValues: {
+          ":image": model.image,
+          ":team": model.team,
+          ":position": model.position,
+          ":league": model.league,
+          ":name": model.name,
+          ":agency": model.agency,
+          ":onlineStatus": model.onlineStatus,
+        },
+        ExpressionAttributeNames: {
+          "#name": "name",
+        },
+      };
+    case "teams":
+      return {
+        UpdateExpression: `
+      SET
+      image = :image,
+      badge = :badge,
+      players = :players,
+      league = :league,
+      #name = :name,
+      limit = :limit,
+      record = :record,
+      active = :active,
+    `,
+        ExpressionAttributeValues: {
+          ":image": model.image,
+          ":badge": model.badge,
+          ":players": model.players,
+          ":league": model.league,
+          ":name": model.name,
+          ":limit": model.limit,
+          ":record": model.record,
+          ":active": model.active,
+        },
+        ExpressionAttributeNames: {
+          "#name": "name",
+        },
+      };
+    case "leagues":
+      return {
+        UpdateExpression: `
+      SET
+      image = :image,
+      badge = :badge,
+      teams = :teams,
+      seasonId = :seasonId,
+      #name = :name,
+      limit = :limit,
+    `,
+        ExpressionAttributeValues: {
+          ":image": model.image,
+          ":badge": model.badge,
+          ":teams": model.teams,
+          ":seasonId": model.seasonId,
+          ":name": model.name,
+          ":limit": model.limit,
+        },
+        ExpressionAttributeNames: {
+          "#name": "name",
+        },
+      };
+  }
+};
+
+module.exports = createExpressionConfig;
---
+++ index.js
@@ -0,0 +1,63 @@
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const {
+  PutCommand,
+  DynamoDBDocumentClient,
+  UpdateCommand,
+  DeleteCommand,
+} = require("@aws-sdk/lib-dynamodb");
+const createExpressionConfig = require("./createExpressionConfig");
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+exports.handler = async (event, context) => {
+  const { eventName, tableName, model } = event;
+  let command;
+
+  switch (eventName) {
+    case "create":
+      command = new PutCommand({
+        TableName: tableName,
+        Item: model,
+      });
+      break;
+    case "update":
+      command = new UpdateCommand({
+        TableName: tableName,
+        Key: {
+          id: model.id,
+        },
+        ...createExpressionConfig(tableName, model),
+        ReturnValues: "ALL_NEW",
+      });
+      break;
+    case "delete":
+      command = new DeleteCommand({
+        TableName: tableName,
+        Key: {
+          id: model,
+        },
+      });
+      break;
+    default:
+      // TODO: return status code here
+      command = null;
+      break;
+  }
+
+
+  try {
+    const response = await docClient.send(command);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        data: response,
+      }),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal Server Error" }),
+    };
+  }
+};
